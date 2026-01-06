5649f14bd0db5ef835ff145e33aa342ecbdbadef
sunJ0120 <sspure1214@gmail.com>
2026-01-07 00:56:12 +0900

    feat: 자동차 이름을 나누기 위한 parser 구현 완료

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -1,4 +1,17 @@
 package racingcar.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputParser {
+    // , 으로 나누는 기능
+    public List<String> parse(String input) {
+        String[] cars = input.split(",");
+
+        if (cars.length <= 1) {
+            throw new IllegalArgumentException("[ERROR] 경주하는 자동차는 한 대 이상이어야 합니다.");
+        }
+
+        return Arrays.stream(cars).toList();
+    }
 }
