a6e45a08bf016c220ecf3db3e6dbdafb9e9828a2
sunJ0120 <sspure1214@gmail.com>
2026-01-07 10:50:09 +0900

    test: 파싱 테스트 추가

--- src/test/java/racingcar/util/InputParserTest.java
+++ src/test/java/racingcar/util/InputParserTest.java
@@ -1,4 +1,67 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/*
+1. ,으로 나눠져서 잘 들어가는지
+2. 자동차가 한 대 이하일때 에러
+ */
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class InputParserTest {
-  
+    private InputParser inputParser = new InputParser();
+
+    @Test
+    void 자동차는_쉼표로_나눠진다() {
+        //given
+        String input = "꼬링크,럭시오,렌트라";
+
+        //when
+        List<String> parsed = inputParser.parse(input);
+
+        //then
+        Assertions.assertThat(parsed)
+                .containsExactlyInAnyOrder("꼬링크","럭시오","렌트라");
+    }
+
+    @Test
+    void 자동차는_한_대_이상이어야_한다() {
+        //given
+        String input = "기라티나";
+
+        //when
+
+        //then
+        Assertions.assertThatThrownBy(() -> inputParser.parse(input))
+                .isInstanceOf(IllegalArgumentException.class)    // 예외가 나오는 조건을 안에 넣는다.
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    void 쉼표만_있으면_예외발생() {
+        //given
+
+        //when
+
+        //then
+        Assertions.assertThatThrownBy(() -> inputParser.parse(",,,"))
+                .isInstanceOf(IllegalArgumentException.class)    // 예외가 나오는 조건을 안에 넣는다.
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    void 빈_문자열은_예외발생() {
+        //given
+
+        //when
+
+        //then
+        Assertions.assertThatThrownBy(() -> inputParser.parse(""))
+                .isInstanceOf(IllegalArgumentException.class)    // 예외가 나오는 조건을 안에 넣는다.
+                .hasMessageContaining("[ERROR]");
+    }
 }
